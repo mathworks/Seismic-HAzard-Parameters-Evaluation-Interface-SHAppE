766b38437e89b0fe0b50eed41817ab900b19251d
Andrew Redfearn <aredfear@mathworks.com>
2026-04-21 15:44:28 +0100

    Edits to meta data

--- tbx/shappe/Contents.m
+++ tbx/shappe/Contents.m
@@ -1,7 +1,7 @@
 % SHAppE: Seismic HAzard Parameters Evaluation Interface
-% Version 1.2.1 (R2025a) 03-Dec-2025
+% Version 1.3.0 (R2026a) 21-Apr-2026
 
-% Copyright 2024-2025 The MathWorks, Inc.
+% Copyright 2024-2026 The MathWorks, Inc.
 
 % App
 %  SHAppE
